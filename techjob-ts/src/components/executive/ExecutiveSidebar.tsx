d1f6afc503dba80cde24fa438cc227df2cee6fc0
Phuri Patphai <thomxsi2005@gmail.com>
2025-10-25 14:59:47 +0700

    super login update

---
+++ techjob-ts/src/components/executive/ExecutiveSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExecutiveSidebar = () => {
+  return (
+    <div>ExecutiveSidebar</div>
+  )
+}
+
+export default ExecutiveSidebar
---
+++ techjob-ts/src/pages/executive/ExDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExDashboard = () => {
+  return (
+    <div>ExDashboard</div>
+  )
+}
+
+export default ExDashboard
